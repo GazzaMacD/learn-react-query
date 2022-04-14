9ad7f38bc98d47a4607b7a10aa5a56afcebc6694
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-04-14 05:56:45 +0200

    First lesson, why react query complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
-import { Random } from "./components/Random";
+import {
+    Random,
+    RandomErr,
+    RandomUpdate,
+    RandomReactQuery,
+} from "./components/Random";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">Learn React Query</header>
             <main>
-                <Random />
+                <div>
+                    <h2>Lesson One: Why React Query</h2>
+                    <Random />
+                    <RandomErr />
+                    <RandomUpdate />
+                    <br></br>
+                    <RandomReactQuery />
+                </div>
             </main>
         </div>
     );
